0729c2eb85b44f8daf4d92d928b96e539a77f0f2
Dave K <dkorff12@gmail.com>
2019-01-28 22:09:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay(musicians, instruments) {
 }
 
 function johnLennonFacts(facts) {
-  var i = facts.length
+  var i = 0
   while (i < facts.length) {
     facts[i] = facts[i] + '!!!'
     i++
