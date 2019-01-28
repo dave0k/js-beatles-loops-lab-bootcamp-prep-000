6a2be0f08d275f8b18560bc22d8cec702d338318
Dave K <dkorff12@gmail.com>
2019-01-28 22:05:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function theBeatlesPlay(musicians, instruments) {
   var array = [];
   var i = 0;
   for (i = 0; i < musicians.length; i++) {
-    array.push(`${musicians} plays ${instruments})
+    array.push(musicians[i] + " plays " + instruments[i])
   }
+  return array
 }
